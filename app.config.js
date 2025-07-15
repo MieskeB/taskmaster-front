6e73b4882b2c743881559a9f2137748289e61c4d
Michel Bijnen <info@michelbijnen.nl>
2025-07-15 16:39:35 +0200

    feat: added app building

---
+++ app.config.js
@@ -0,0 +1,15 @@
+export default {
+    expo: {
+        name: "taskmaster-front",
+        slug: "taskmaster-front",
+        icon: "./assets/icon.png",
+        android: {
+            package: "nl.michelbijnen.taskmaster"
+        },
+        extra: {
+            eas: {
+                projectId: "7bd3af09-7789-4786-a84b-5fdc23528580"
+            }
+        },
+    }
+};
